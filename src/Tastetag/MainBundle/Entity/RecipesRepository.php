7afe836fd2a6d1e5496fd429c91fc2d8f2630623
Agata Matoga <agatka.ma@gmail.com>
2015-02-15 17:48:44 +0100

    tags search fixed

--- src/Tastetag/MainBundle/Entity/RecipesRepository.php
+++ src/Tastetag/MainBundle/Entity/RecipesRepository.php
@@ -3,6 +3,8 @@
 namespace Tastetag\MainBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+
 
 class RecipesRepository extends EntityRepository
 {
@@ -36,37 +38,43 @@ class RecipesRepository extends EntityRepository
             ->getResult();
     }
 
-    public function findByTags($tags)
+    public function findByTags($tags,$number)
     {
           $sql = " 
-           SELECT r.id
-            FROM   recipes r
-             INNER JOIN (SELECT rt.recipe_id
-                         FROM recipes_tags rt
-                                  INNER JOIN recipes r
-                                    ON r.id = rt.recipe_id
-                                  INNER JOIN tags t
-                                    ON t.id = rt.tag_id
-                         WHERE    t.name IN (".$tags.")
-                         GROUP BY rt.recipe_id)
-                          aa
-               ON r.id = aa.recipe_id
-        ";
+            SELECT r.id
+FROM   recipes r
+ INNER JOIN (SELECT   rt.recipe_id
+             FROM     recipes_tags rt
+                      INNER JOIN recipes r
+                        ON r.id = rt.recipe_id
+                      INNER JOIN tags t
+                        ON t.id = rt.tag_id
+             WHERE    t.name IN (".$tags.")
+             GROUP BY rt.recipe_id
+             HAVING   Count(rt.recipe_id) =".$number.") aa
+   ON r.id = aa.recipe_id";
+
 
         $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
         $stmt->execute();
 
         $recipes = $stmt->fetchAll();
 
-        $recipeIds = array();
-        foreach( $recipes as $recipe) {    
-            array_push($recipeIds, $recipe['id']);
-        }
-        $tagIdsStr = implode(',', $recipeIds);
+        if($recipes != null) {
+            $recipeIds = array();
+            foreach( $recipes as $recipe) {    
+                array_push($recipeIds, $recipe['id']);
+            }
+            $recipeIdsStr = implode(',', $recipeIds);
 
-        return $this->getEntityManager()
-            ->createQuery(
-                 'SELECT r FROM TastetagMainBundle:Recipes r WHERE r.id IN (:ids)')->setParameter('ids', $tagIdsStr)
-            ->getResult();
+            $qb = $this->getEntityManager()->createQueryBuilder();
+            $qb->select('r')
+            ->from('TastetagMainBundle:Recipes', 'r')
+            ->add('where', $qb->expr()->in('r.id', $recipeIdsStr));
+
+            return $qb->getQuery()->getResult();
+        } else {
+            return $recipes;
+        }
     }
 }
